2eecd0f25fa8cb01c6dc2343aaa6cb9d707e033f
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-09-01 19:10:58 +0100

    Update index.js

--- JS OOP/index.js
+++ JS OOP/index.js
@@ -48,3 +48,16 @@ const message = "Don't be sad, be happy!";
 let withSlice = message.slice(0, 3);
 console.log(withSlice);
 */
+
+let message = "";
+
+if (message === "spanish") {
+  console.log("hola mundo");
+} else {
+  if (message === "french") {
+    console.log("bonjour tout le monde");
+  } else {
+    console.log("hello world");
+  }
+}
+
