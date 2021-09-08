3d0474d96137cabfaf58b136b6b9773c58030c09
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-09-08 10:37:28 +0100

    Update index.js

--- JS OOP/index.js
+++ JS OOP/index.js
@@ -60,6 +60,13 @@ if (message === "spanish") {
     console.log("hello world");
   }
 }
-
 */
 
+
+/*
+Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase()
+}
+*/
