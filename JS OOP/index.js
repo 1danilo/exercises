e454d96dd6d562e2d37b883a634406878c168a3e
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2021-09-15 09:32:58 -0300

    Update index.js

--- JS OOP/index.js
+++ JS OOP/index.js
@@ -94,3 +94,16 @@ It should look like this:
 Sam Harris => S.H
 Patrick Feeney => P.F
 */
+
+
+Beginner - Lost Without a Map
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+
+  }
+
