4a45ecd6d04ebfd526341c480d867b36364ffcd7
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-03-18 16:36:40 -0300

    Add files via upload

--- teste17fev/src/FileCabinet/SuiteScripts/Suitelet1.js
+++ teste17fev/src/FileCabinet/SuiteScripts/Suitelet1.js
@@ -9,89 +9,115 @@ define(["N/search", "N/ui/serverWidget"], function (search, serverWidget) {
       title: "Simple Form",
     });
 
+    form.addButton({
+      id: "custpage-button_example",
+      label: "Test",
+      functionName: "(function(){ window.open('www.google.com', '_blank'); })",
+    });
+
+    form.addSubmitButton({
+      label: "Enviar",
+    });
+
+    const selectField = form.addField({
+      id: "custpage_abc_text",
+      type: serverWidget.FieldType.SELECT,
+      label: "Campo Select",
+    });
+
+    selectField.addSelectOption({
+      value: "",
+      text: "",
+    });
+
+    selectField.addSelectOption({
+      value: "a",
+      text: "Albert",
+    });
+
     context.response.writePage({ pageObject: form });
   }
 
-  function exemploBuscaCustomer() {
-    const results = search
-      .create({
-        type: search.Type.CUSTOMER,
-        filters: [
-          {
-            name: "phone",
-            operator: search.Operator.STARTSWITH,
-            values: "(031)",
-          },
-        ],
-        columns: [
-          {
-            name: "companyname",
-          },
-          {
-            name: "phone",
-          },
-        ],
-      })
-      .run()
-      .getRange({
-        start: 0,
-        end: 1000,
-      })
-      .map(function (result) {
-        const columns = result.columns;
-        const firstName = result.getValue(columns[0]);
-        return {
-          firstName: result.getValue(columns[0]),
-          phone: result.getValue(columns[1]),
-        };
-      });
+  // function exemploBuscaCustomer() {
+  //   const results = search
+  //     .create({
+  //       type: search.Type.CUSTOMER,
+  //       filters: [
+  //         {
+  //           name: "phone",
+  //           operator: search.Operator.STARTSWITH,
+  //           values: "(031)",
+  //         },
+  //       ],
+  //       columns: [
+  //         {
+  //           name: "companyname",
+  //         },
+  //         {
+  //           name: "phone",
+  //         },
+  //       ],
+  //     })
+  //     .run()
+  //     .getRange({
+  //       start: 0,
+  //       end: 1000,
+  //     })
+  //     .map(function (result) {
+  //       const columns = result.columns;
+  //       const firstName = result.getValue(columns[0]);
+  //       return {
+  //         firstName: result.getValue(columns[0]),
+  //         phone: result.getValue(columns[1]),
+  //       };
+  //     });
 
-    context.response.write({ output: JSON.stringify(results) });
-  }
+  //   context.response.write({ output: JSON.stringify(results) });
+  // }
 
-  function exemploBuscas() {
-    const mySearch = search.create({
-      type: search.Type.CUSTOMER,
-      filters: [
-        {
-          name: "phone",
-          operator: search.Operator.STARTSWITH,
-          values: "(031)",
-        },
-      ],
-      columns: [
-        {
-          name: "firstname",
-        },
-      ],
-    });
+  // function exemploBuscas() {
+  //   const mySearch = search.create({
+  //     type: search.Type.CUSTOMER,
+  //     filters: [
+  //       {
+  //         name: "phone",
+  //         operator: search.Operator.STARTSWITH,
+  //         values: "(031)",
+  //       },
+  //     ],
+  //     columns: [
+  //       {
+  //         name: "firstname",
+  //       },
+  //     ],
+  //   });
 
-    const searchResultSet = mySearch.run();
+  //   const searchResultSet = mySearch.run();
 
-    const results = searchResultSet.getRange({ start: 0, end: 1000 });
+  //   const results = searchResultSet.getRange({ start: 0, end: 1000 });
 
-    // Limite 1000 resultados
-    results.forEach(function (result) {
-      //handle result
-    });
+  //   // Limite 1000 resultados
+  //   results.forEach(function (result) {
+  //     //handle result
+  //   });
 
-    // Limite 4000 resultados
-    searchResultSet.each(function (result) {
-      //handle result
-      return true;
-    });
+  //   // Limite 4000 resultados
+  //   searchResultSet.each(function (result) {
+  //     //handle result
+  //     return true;
+  //   });
 
-    // sem limite de resultados
-    var myPagedData = mySearch.runPaged();
+  //   // sem limite de resultados
+  //   var myPagedData = mySearch.runPaged();
 
-    myPagedData.pageRanges.forEach(function (pageRange) {
-      var myPage = myPagedData.fetch({ index: pageRange.index });
+  //   myPagedData.pageRanges.forEach(function (pageRange) {
+  //     var myPage = myPagedData.fetch({ index: pageRange.index });
 
-      myPage.data.forEach(function (result) {
-        //handle result
-      });
-    });
-  }
+  //     myPage.data.forEach(function (result) {
+  //       //handle result
+  //     });
+  //   });
+  // }
 
   return {
     onRequest: onRequest,
---
+++ teste17fev/src/FileCabinet/SuiteScripts/Suitelet2.js
@@ -0,0 +1,12 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+define(["N/search"], function (search, serverWidget) {
+  function onRequest(context) {}
+
+  return {
+    onRequest: onRequest,
+  };
+});
