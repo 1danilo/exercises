f4e28be39041befa6deb58caa06af8cffb5499d6
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-03-23 15:11:04 -0300

    Add files via upload

--- scripts/validation cpfCnpj/src/FileCabinet/SuiteScripts/ps_cl_validation_cpf_cnpj.js
+++ scripts/validation cpfCnpj/src/FileCabinet/SuiteScripts/ps_cl_validation_cpf_cnpj.js
@@ -3,25 +3,26 @@
  *@NScriptType ClientScript
  */
 define(["N/search"], function (search) {
-  function saveRecord(context) {}
-
-  function validateField(context) {}
+  function saveRecord(context) {
+    // usar um if para validar o retorno da funcao buscaCnpj e bloquear a criação desse registro
+  }
 
   function fieldChanged(context) {
-    const fieldId = context.fieldId;
-    const currentRecord = context.currentRecord;
-    const recordType = currentRecord.type;
+    const fieldId = context.fieldId; // traz o campo que esta sendo alterado pelo usuario
+    const currentRecord = context.currentRecord; // criamos uma variavel para definir o registro atual (como um todo)
+    const recordType = currentRecord.type; // criamos uma variavel para definir o tipo de registro atual (qual o tipo desse registro)
 
+    // o objeto foi criado para ser usado como parametro para a função buscaCnpjCpf
     const objeto = {
       type: recordType,
       name: fieldId,
-      value: currentRecord.getValue({ fieldId: fieldId }),
+      value: currentRecord.getValue({ fieldId: fieldId }), // é uma função do currentRecord que retorna o valor do campo
     };
 
     if (fieldId === "custentity_psg_br_cnpj") {
       if (!currentRecord.getValue({ fieldId: fieldId })) return true;
       // window.alert("CNPJ já cadastrado!");
-      const retorno = buscaCnpjCpf(objeto);
+      const retorno = buscaCnpjCpf(objeto); // declaramos uma constante para receber o retorno da nossa função buscaCnpjCpf
       if (retorno != "") {
         window.alert("CNPJ já cadastrado!");
       }
@@ -37,42 +38,45 @@ define(["N/search"], function (search) {
   }
 
   function buscaCnpjCpf(objeto) {
-    var retorno = "";
-    const results = search
+    let retorno = ""; // declaramos uma variável de retorno para verificar a existência do cnpj ou cpf
 
+    search
       .create({
-        type: objeto.type,
+        type: objeto.type, // tipo de registro para realizar a busca
         filters: [
           {
-            name: objeto.name,
-            operator: search.Operator.STARTSWITH,
-            values: objeto.value,
+            name: objeto.name, // id do campo
+            operator: search.Operator.STARTSWITH, // tipo de operador a ser utilizado na busca
+            values: objeto.value, // valor a ser buscado no campo do registro
           },
         ],
         columns: [
           {
-            name: "internalid",
+            name: "internalid", // valor do campo do registro
           },
         ],
       })
-      .run()
+      .run() // executa a busca
       .getRange({
+        // define o range
         start: 0,
         end: 1000,
       })
       .forEach(function (result) {
-        retorno = result.getValue({ name: "internalid" });
+        //percorre os resultados
+        retorno = result.getValue({ name: "internalid" }); // adicionamos o valor do campo "internalid" do registro encontrado
       });
-    return retorno;
+    return retorno; //retorno da função com o resultado da busca
   }
 
   // 1-criar essa mesma condicao para o cpf
   // 2-fazer uma busca pelo cnpj ou cpf digitado(pegar o valor digitado pelo usuario e declarar numa variavel - currentRecord.getValue)
   // 3-criar um alerta se o registro existir
 
+  // falar com o guilherme e apresentar a solução até o momento e questionar se atende o que foi pedido pelo cliente
+
   return {
-    // saveRecord: saveRecord,
-    // validateField: validateField,
+    saveRecord: saveRecord,
     fieldChanged: fieldChanged,
   };
 });
