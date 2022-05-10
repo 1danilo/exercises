8cf6cfd3f091db6c04cbbaa8a89347d517a14745
Danilo Mendes <57329055+mendesdanilo@users.noreply.github.com>
2022-05-10 08:50:05 -0300

    Add files via upload

--- scripts/ps_sc_bank_statement_import.js
+++ scripts/ps_sc_bank_statement_import.js
@@ -2,13 +2,15 @@
  *@NApiVersion 2.x
  *@NScriptType ScheduledScript
  */
-define(['N/runtime', 'N/search', 'N/record', 'N/file'], function(runtime, search, record, file) {
+define(["N/runtime", "N/search", "N/record", "N/file"], function (
+  runtime,
+  search,
+  record,
+  file
+) {
+  function execute(context) {}
 
-    function execute(context) {
-        
-    }
-
-    return {
-        execute: execute
-    }
+  return {
+    execute: execute,
+  };
 });
